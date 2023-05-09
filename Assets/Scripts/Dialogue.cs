990b60f0815b8cf340dadb61068273b8b9902537
tessla-muir <84484852+tessla-muir@users.noreply.github.com>
2023-05-08 21:47:38 -0400

    Callbacks to update UI & current dialogue for Editor

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -7,6 +7,21 @@ namespace Game.Dialogue
     [CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue", order = 0)]
     public class Dialogue : ScriptableObject
     {
-        [SerializeField] DialogueNode[] nodes;
+        [SerializeField] List<DialogueNode> nodes = new List<DialogueNode>();
+
+#if UNITY_EDITOR 
+        private void Awake()
+        {
+            if (nodes.Count == 0)
+            {
+                nodes.Add(new DialogueNode());
+            }
+        }
+#endif
+
+        public IEnumerable<DialogueNode> GetAllNodes()
+        {
+            return nodes;
+        }
     }
 }
--- Assets/Scripts/Editor/DialogueEditor.cs
+++ Assets/Scripts/Editor/DialogueEditor.cs
@@ -8,6 +8,9 @@ namespace Game.Dialogue.Editor
 {
     public class DialogueEditor : EditorWindow
     {
+        Dialogue selectedDialogue = null;
+
+        // Creates dialogue window
         [MenuItem("Window/Dialogue Editor")]
         private static void ShowEditorWindow()
         {
@@ -16,9 +19,11 @@ namespace Game.Dialogue.Editor
             window.Show();
         }
 
+        // Calls editor if a Dialogue SO is opened
         [OnOpenAsset(1)]
         public static bool OnOpenAsset(int instanceID, int line)
         {
+            // Get object from instance ID
             Dialogue dialogue = EditorUtility.InstanceIDToObject(instanceID) as Dialogue;
 
             if (dialogue != null)
@@ -29,9 +34,31 @@ namespace Game.Dialogue.Editor
             return false;
         }
 
-        private void OnGUI()
-        {
+        // Called when an object becomes enabled and active
+        private void OnEnable() {
+            Selection.selectionChanged += OnSelectionChanged;
+        }
 
+        private void OnSelectionChanged()
+        {
+            Dialogue newDialogue = Selection.activeObject as Dialogue;
+            if (newDialogue != null)
+            {
+                selectedDialogue = newDialogue;
+                Repaint(); // Calls OnGUI
+            }
+        }
+
+        private void OnGUI() {
+            if (selectedDialogue == null)
+            {
+                EditorGUILayout.LabelField("No Dialogue Selected.");
+            }
+            else
+            {
+                EditorGUILayout.LabelField(selectedDialogue.name);
+            }
         }
     }
 }
+
