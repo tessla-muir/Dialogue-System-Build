c50acc223ac90ed8d4587e0c1a6533c5b4baf7a7
Tessla Muir <tesslamuir@yahoo.com>
2023-09-16 17:36:39 -0400

    PlayerConversant & DialogueUI scripts

---
+++ Assets/Scripts/DialogueUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.Dialogue;
+using TMPro;
+
+namespace Game.UI
+{
+    public class DialogueUI : MonoBehaviour
+    {
+        PlayerConversant playerConversant;
+        [SerializeField] TextMeshProUGUI AIText;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            playerConversant = GameObject.Find("Player").GetComponent<PlayerConversant>();
+            AIText.text = playerConversant.GetText();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
